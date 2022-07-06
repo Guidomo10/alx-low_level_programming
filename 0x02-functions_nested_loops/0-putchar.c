48ac615251f748ca6773d595fbfe864184397965
Your Name <you@example.com>
2022-07-05 20:26:38 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <main.h>
+
+/**
+ * main -prints _putchar.
+ * return: always 0 (success).
+ */
+
+int main(void)
+
+{
+int i = "_putchar", j = ("\n");
+
+while (i)
+{
+_putchar(i);
+i++
+}
+while (j)
+{
+_putchar(j);
+j++
+}
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+              Functions, nested loops
+0) This programm that prints _putchar, followed by a new line.
