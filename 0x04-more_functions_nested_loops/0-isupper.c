22720727e171cc1502262905564c3b5d51e279c1
Your Name <you@example.com>
2022-07-07 19:13:55 -0700

    0. isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,11 +2,15 @@
 
 /**
  * _isupper - a function that checks for uppercase character
- * @c: an integer parameter
- * Return: somethings but success
+ * @c: input
+ * Return: 1 if c is uppercase, 0 otherwise
  */
 
 int _isupper(int c)
+
 {
-return : ((c >= 'A') && (c <= 'Z'));
+if ((c >= 'A') && (c <= 'Z'))
+return (1);
+else
+return (0);
 }
