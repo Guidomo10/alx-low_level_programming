3608b00002737a8157a012d2917fe93bbd92fe9e
Your Name <you@example.com>
2022-07-15 16:39:07 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times in lowercase.
+ *
+ * Return: void.
+ */
+
+void print_alphabet_x10(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
