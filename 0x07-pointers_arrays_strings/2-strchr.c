c5172b7c3ec2fe9ee6d607f0113168aff5747486
Your Name <you@example.com>
2022-07-18 09:06:20 -0700

    2. strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strchr -  locates a character in a string.
+ * @c: character in the string
+ * @s: string
+ *
+ * Return: pointer to character or NULL if not found.
+ */
+
+char *_strchr(char *s, char c)
+{
+int a = 0, b;
+while (s[a])
+{
+a++;
+}
+while (s[a])
+{
+if (c == s[b])
+{
+s += b;
+return (s);
+}
+}
+return ('\0');
+}
