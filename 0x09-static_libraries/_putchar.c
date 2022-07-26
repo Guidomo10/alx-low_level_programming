9b94606ae06fceffc542084310c59b0719ce7e9f
Your Name <you@example.com>
2022-07-26 11:30:21 -0700

    0. A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,21 +1,13 @@
 #include "main.h"
 
 /**
- * main - Entry points.
+ * _putchar - print character c.
+ *@c: character to print
  *
  * Return: always 0 (success).
  */
 
-int main(void)
+int _putchar(char c)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
 return (0);
 }
