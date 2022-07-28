c0780c858b2b7ef8d1d7080a8f845d6a40a60ecb
Your Name <you@example.com>
2022-07-27 17:11:04 -0700

    0. It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - prints the name of the programm
+ * @argc: integger.
+ * @argv: string.
+ *
+ * Return: integger.
+ */
+
+int main(int argc, char *argv[])
+{
+if (argc == 1)
+{
+printf("%s\n", argv[0]);
+}
+return (0);
+}
