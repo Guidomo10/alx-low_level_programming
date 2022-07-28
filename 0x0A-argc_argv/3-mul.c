c04f57ced39f6ae9a8700540cb63ca143834c35c
Your Name <you@example.com>
2022-07-27 17:43:55 -0700

    3. Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - multiplies two numbers.
+ * @argc: integger.
+ * @argv: string.
+ *
+ * Return: integger or error if programm doesn't receive two arguments.
+ */
+
+int main(int argc, char *argv[])
+{
+int m, n, p;
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+else
+{
+m = atoi(argv[1]);
+n = atoi(argv[2]);
+p = i * j;
+printf("%d\n", p);
+}
+return (0);
+}
